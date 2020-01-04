71f1e1795f5bef44914c238eab26f09fff1d242f
Athena Pnevmatikaki <a.pnevmatikaki@gmail.com>
2020-01-04 13:27:16 +0200

    Next and previous page functions

--- src/MainFrame.py
+++ src/MainFrame.py
@@ -17,10 +17,13 @@ class MainFrame(Frame):
             self.nrows = 3
         self.frames = []
         self.flower_shop = data
-        self.init_main_frame()
         self.login_dialog = None
         self.register_dialog = None
         self.info_dialog = None
+        self.start = 0
+        self.page = 0
+        self.pages = 0
+        self.init_main_frame()
 
     def init_main_frame(self):
         self.root.title(self.flower_shop.name)
@@ -32,11 +35,10 @@ class MainFrame(Frame):
                 frame = Frame(self.root)
                 frame.grid(row=row, column=col, padx=5, pady=5)
                 self.frames.append(frame)
+        self.pages = int((len(self.flower_shop.bouquets) + 1) / (self.ncols * self.nrows))
         self.display_menu()
 
     def display_bouquet(self, bouquet, frame_index):
-        for widget in self.frames[frame_index].winfo_children():
-            widget.destroy()
         Label(self.frames[frame_index], text=bouquet.name).pack()
         img = Image.open("../photos/"+bouquet.image)
         img = img.resize((130, 130), Image.ANTIALIAS)
@@ -58,6 +60,12 @@ class MainFrame(Frame):
         menu.add_cascade(label="Bouquets")
         info.add_command(label="Contact Us", command=self.information)
         menu.add_cascade(label="Info", menu=info)
+        view = Menu(menu)
+        menu.add_cascade(label="View", menu=view)
+        view.add_command(label="2 x 2", command=lambda: self.change_grid(2, 2))
+        view.add_command(label="3 x 2", command=lambda: self.change_grid(3, 2))
+        view.add_command(label="4 x 3", command=lambda: self.change_grid(4, 3))
+        view.add_command(label="5 x 3", command=lambda: self.change_grid(5, 3))
         user = Menu(menu)
         if self.flower_shop.logged_user is None:
             menu.add_cascade(label="User", menu=user)
@@ -66,10 +74,23 @@ class MainFrame(Frame):
         else:
             menu.add_cascade(label=self.flower_shop.logged_user.username, menu=user)
             user.add_command(label="Log out", command=self.logout)
-        counter = 0
-        for bouquet in self.flower_shop.bouquets:
-            self.display_bouquet(bouquet, counter)
-            counter += 1
+        for frame in self.frames:
+            for widget in frame.winfo_children():
+                widget.destroy()
+        j = 0
+        for i in range(self.start, self.start+self.nrows*self.ncols-1):
+            if i >= len(self.flower_shop.bouquets):
+                break
+            j = i
+            self.display_bouquet(self.flower_shop.bouquets[i], i-self.start)
+        if self.flower_shop.logged_user is not None:
+            Button(self.frames[j-self.start+1], text="Create bouquet", bg="RosyBrown2", width="12", height="1").pack()
+        if self.page > 0:
+            Button(self.frames[j - self.start+1], text="Previous Page", bg="RosyBrown2", width="12", height="1",
+                   command=lambda: self.previous_page()).pack()
+        if self.page < self.pages and len(self.flower_shop.bouquets) >= self.ncols*self.nrows:
+            Button(self.frames[j - self.start+1], text="Next Page", bg="RosyBrown2", width="12", height="1",
+                   command=lambda: self.next_page()).pack()
 
     def login(self):
         self.login_dialog = Toplevel()
@@ -112,4 +133,25 @@ class MainFrame(Frame):
 
     def logout(self):
         self.flower_shop.logged_user = None
+        self.display_menu()
+
+    def change_grid(self, ncols, nrows):
+        print("changing grid to "+str(ncols)+"x"+str(nrows))
+        self.frames = []
+        self.ncols = ncols
+        self.nrows = nrows
+        for widget in self.root.winfo_children():
+            widget.destroy()
+        self.page = 0
+        self.start = 0
+        self.init_main_frame()
+
+    def next_page(self):
+        self.page += 1
+        self.start = self.start + self.nrows*self.ncols - 1
+        self.display_menu()
+
+    def previous_page(self):
+        self.page -= 1
+        self.start = self.start - self.nrows*self.ncols + 1
         self.display_menu()
