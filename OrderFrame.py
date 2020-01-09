2656aafd3e2c176bc5e3763110a36d6aa9adb2a3
marosimosi <58637328+marosimosi@users.noreply.github.com>
2020-01-09 22:20:02 +0200

    Add files via upload

--- OrderFrame.py
+++ OrderFrame.py
@@ -15,7 +15,7 @@ class OrderFrame(Frame):
 
     def init_order_frame(self):
         self.root.title("Order")
-        self.root.configure(bd=10)
+        self.root.configure(bd=10, height=10, width=30)
         self.root.columnconfigure((0, 1, 2), weight=1)
         self.root.rowconfigure((0, 2), weight=1)
         self.root.rowconfigure(1, weight=2)
@@ -26,12 +26,55 @@ class OrderFrame(Frame):
         Label(self.root, text="Delivery Time and Date").grid(row=2, column=0)
         Entry(self.root, textvariable=self.delivery, width=40).grid(row=2, column=1, columnspan=2)
         Button(self.root, text="Buy", bg="LightSteelBlue2", width="6", height="1",
-               command=lambda: self.parent.on_confirmed_buy(self.bouquet_id, self.address,
-                                                            self.credit_card)).grid(row=4, column=1)
+               command=lambda: self.check_credit_card(self.credit_card)).grid(row=4, column=1)
         Button(self.root, text="Cancel", bg="LightSteelBlue2", width="6", height="1",
                command=lambda: self.on_closing()).grid(row=4, column=2)
         self.root.protocol('WM_DELETE_WINDOW', self.on_closing)
 
     def on_closing(self):
         self.parent.on_cancelled_buy()
+
+    def check_credit_card(self, credit_card):
+        a = str(credit_card.get())
+        while True:
+            x = 0
+            li = []
+            s = 0
+            check=''
+            for i in range(len(a)):
+                if a[i].isdigit():
+                    li = li + [int(a[i])]
+                else:
+                    x = 1
+            if len(li) != 16:
+                check = 'false'
+                break
+            for i in range(4, 15, 5):
+                if a[i] == ' ' or a[i] == '-': x = 0
+            if x == 1:
+                check = 'false'
+                break
+            if li[0] < 4 or li[0] > 7:
+                check = 'false'
+                break
+            else:
+                for i in range(0, 15, 2):
+                    li[i] = li[i] * 2
+                    if li[i] > 9:
+                        b = str(li[i])
+                        li[i] = int(b[0]) + int(b[1])
+                for i in li:
+                    s = s + i
+                if s % 10 == 0:
+                    check = 'true'
+                    break
+                else:
+                    check = 'false'
+                    break
+        if check == 'false':
+            Label(self.root, text="Wrong credit card number").grid(row=3, column=1)
+        elif check == 'true':
+            self.parent.on_confirmed_buy(self.bouquet_id, self.address, self.credit_card)
+
+
 
